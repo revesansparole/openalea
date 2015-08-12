95925e86c27c51da9aba7d7c39f60eab26be84dd
revesansparole <revesansparole@gmail.com>
2015-08-12 09:14:44 +0200

    added untested for node

--- core/src/core/node_control_flow.py
+++ core/src/core/node_control_flow.py
@@ -20,6 +20,7 @@ __license__ = "Cecill-C"
 __revision__ = " $Id$ "
 
 
+from interface import IInt
 from node import Node
 from subdataflow import get_upstream_subdataflow
 
@@ -49,10 +50,28 @@ class ControlFlowNode(Node):
         raise NotImplementedError
 
 
+class XNode(ControlFlowNode):
+    """ Does nothing but allow to add arguments
+    in a loop of any sort.
+    """
+
+    control_type = "X"
+
+    def __init__(self):
+        inputs = ({'name': 'key', 'interface': None},
+                  {'name': 'order', 'interface': IInt, 'default': 0})
+        outputs = ({'name': 'out'},)
+
+        ControlFlowNode.__init__(self, inputs, outputs)
+
+    def perform_evaluation(self, algo, env, state, vid):
+        pass
+
+
 class WhileNode(ControlFlowNode):
     """ Implement a type of while on a dataflow.
 
-    While has two entrance "test" and "task". "Task"
+    WhileNode has two input ports "test" and "task". "Task"
     is executed and its result is forwarded on the output
     of this node as long as "test" is true. Once "test" is
     False, "task" is no longer executed and the output
@@ -74,7 +93,7 @@ class WhileNode(ControlFlowNode):
 
         pid_out = df.out_port(vid, "out")
         if pid_out in state and not state.has_changed(pid_out):
-            return  # while already finished previous execution
+            return  # 'while' already finished previous execution
 
         # find subdataflow upstream of 'test' port
         pid_test = df.in_port(vid, "test")
@@ -104,3 +123,65 @@ class WhileNode(ControlFlowNode):
             if pid_out not in state:
                 state.set_data(pid_out, None)
             state.set_changed(pid_out, False)
+
+
+class ForNode(ControlFlowNode):
+    """ Implement a classical python for loop.
+
+    ForNode has two input ports, "iter" and "task".
+    Evaluation will exhaust the provided iterator
+    until StopIteration is raised. Each time the
+    actual iter value is passed to the "task",
+    "task" is executed and the result is mirrored
+    on the output of the node.
+
+    Once the loop is finished, "task" is no longer
+    evaluation and the output of the node stays
+    unchanged on the last computed value.
+    """
+
+    control_type = "for"
+
+    def __init__(self):
+        inputs = ({'name': 'iter', 'interface': None},
+                  {'name': 'task', 'interface': None})
+        outputs = ({'name': 'out'},)
+
+        ControlFlowNode.__init__(self, inputs, outputs)
+        self.set_lazy(False)
+
+    def perform_evaluation(self, algo, env, state, vid):
+        df = algo.dataflow()
+
+        pid_out = df.out_port(vid, "out")
+        if pid_out in state and not state.has_changed(pid_out):
+            return  # 'for' already finished previous execution
+
+        # find subdataflow upstream of 'iter' port
+        pid_iter = df.in_port(vid, "iter")
+        sub = get_upstream_subdataflow(df, pid_iter)
+
+        # eval
+        ss_iter = state.clone(sub)
+        algo_iter = algo.clone(sub)
+        try :
+            algo_iter.eval(env, ss_iter)
+        except StopIteration:
+            if pid_out not in state:
+                state.set_data(pid_out, None)
+            state.set_changed(pid_out, False)
+
+        state.update(ss_iter)
+
+        # find subdataflow upstream 'task' port
+        pid_task = df.in_port(vid, "task")
+        sub = get_upstream_subdataflow(df, pid_task)
+
+        #eval
+        ss_task = state.clone(sub)
+        algo_task = algo.clone(sub)
+        algo_task.eval(env, ss_task)
+        state.update(ss_task)
+
+        # fill ports
+        state.set_data(pid_out, state.get_data(pid_task))
